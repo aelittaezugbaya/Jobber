d1f82aecaf8a477ebf9cbd593df0c6e589ab697c
Ramon Brand <ramon2009@gmail.com>
2017-09-11 12:17:17 +0300

    Initial renaming and cleaning up.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const routes_index = require('./routes/routes_index');
+const routes_api = require('./routes/routes_api');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -18,25 +17,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', index);
-app.use('/users', users);
+app.use('/', routes_index);
+app.use('/api', routes_api);
 
-// catch 404 and forward to error handler
+// -- Catch Error
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// -- Error Handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('Error: ' + err.message);
 });
 
 module.exports = app;
--- routes/routes_api.js
+++ routes/routes_api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Route of Api:');
 });
 
 module.exports = router;
