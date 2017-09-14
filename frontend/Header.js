20b6b6cb13bb4fffda2ab278588b4a44ff7ba2fd
Aelitta Ezugbaya <aelittae@gmail.com>
2017-09-14 17:32:39 +0300

    Added logo

--- frontend/Header.js
+++ frontend/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Tabs from './Tabs';
 import Input from './Input';
 import MenuSide from './MenuSide';
-import IconButton from './IconButton'
+import IconButton from './IconButton';
+import Loggo from './Logo';
 
 
 export default class Header extends React.Component{
@@ -23,8 +24,10 @@ export default class Header extends React.Component{
                             <IconButton classNameA="navbar-brand button-collapse" classNameI="fa-bars fa-lg" onClick={this.onClickMenu} />
                         </div>
                         <Tabs className="tabs-style col s3"/>
-                        <Input className="col s4 push-s1"/>
-
+                        <Input className="col s3 push-s1"/>
+                        <div  className="col s3 push-s4">
+                            <Loggo name="Jobber"/>
+                        </div>
                     </div>
 
                 </div>
